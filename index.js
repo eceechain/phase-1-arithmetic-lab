ffca125aedac0ab96cf793f65c438f0e739c63e7
ecee <you@example.com>
2023-09-21 17:17:46 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
+let num1 = 31; 
+let num2 = 2;  
+let multiply = num1 * num2;
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+var num3 = 20; 
+var num4 = 4; 
+var mod = (num3 % num4) + num4;
+var numbers = [10, 5, 15, 20, 8, 12];
+var max = numbers[0];
+for (var i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+ 
+  
 
-// Write your code here
+
+  
+  
+  
+  
+  
+
+
+
